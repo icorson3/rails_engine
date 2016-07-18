220e69bc710803564c8a547126ea62ea438c571b
Ilana Corson <ilana.corson@gmail.com>
2016-07-18 15:00:23 -0600

    merchants find finished

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,14 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'api/v1/merchants#index'
+
+  get 'api/v1/merchants/find' => 'api/v1/merchants/find#show'
+
+  namespace :api do
+    namespace :v1 do
+      resources :merchants, only: [:index, :show], defaults: { format: :json }
+
+    end
+  end
+
+
 end
