698a7fe94d85385c384688d36fc7e130c31158c6
Ilana Corson <ilana.corson@gmail.com>
2016-07-18 21:30:41 -0600

    fixed the unit_price issue

--- app/models/invoice_item.rb
+++ app/models/invoice_item.rb
@@ -1,3 +1,8 @@
 class InvoiceItem < ApplicationRecord
   belongs_to :invoice
+  before_create :unit_price_to_dollars
+
+    def unit_price_to_dollars
+      self.unit_price = ((unit_price.to_i.to_f)/100).round(2)
+    end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160719022438) do
+ActiveRecord::Schema.define(version: 20160719032435) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,7 +26,7 @@ ActiveRecord::Schema.define(version: 20160719022438) do
     t.integer  "item_id"
     t.integer  "invoice_id"
     t.integer  "quantity"
-    t.decimal  "unit_price"
+    t.string   "unit_price"
     t.datetime "created_at"
     t.datetime "updated_at"
     t.index ["invoice_id"], name: "index_invoice_items_on_invoice_id", using: :btree
