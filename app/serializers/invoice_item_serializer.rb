0b7f4e2a76cef427c176d8a4c8033645a0741bea
Kerry <kerry.sheldon@gmail.com>
2016-07-20 10:28:12 -0600

    revenue

--- app/serializers/invoice_item_serializer.rb
+++ app/serializers/invoice_item_serializer.rb
@@ -2,6 +2,6 @@ class InvoiceItemSerializer < ActiveModel::Serializer
   attributes :id, :invoice_id, :quantity, :item_id, :unit_price
 
   def unit_price
-    (object.unit_price.to_f/100).round(2)
+    ((object.unit_price.to_f/100).round(2)).to_s
   end
 end
--- app/serializers/item_serializer.rb
+++ app/serializers/item_serializer.rb
@@ -2,6 +2,6 @@ class ItemSerializer < ActiveModel::Serializer
   attributes :id, :name, :description, :merchant_id, :unit_price
 
   def unit_price
-    (object.unit_price.to_f/100).round(2)
+    ((object.unit_price.to_f/100).round(2)).to_s
   end
 end
