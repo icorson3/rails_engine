5181d89cb1ae76c273be97ebfdc43515ebfdf329
Kerry <kerry.sheldon@gmail.com>
2016-07-20 18:27:48 -0600

    spec harness passing'

--- app/models/item.rb
+++ app/models/item.rb
@@ -9,27 +9,28 @@ class Item < ApplicationRecord
 
 
   def self.most_revenue(top_num)
-    self.joins(invoice_items: [invoice: [:transactions]]).where("transactions.result != 'failed'").group("items.id").order("sum_invoice_items_quantity_all_invoice_items_unit_price DESC").limit(top_num).sum("invoice_items.quantity * invoice_items.unit_price")
+    result= self.joins(invoice_items: [invoice: [:transactions]]).where("transactions.result != 'failed'").group("items.id").order("sum_invoice_items_quantity_all_invoice_items_unit_price DESC").limit(top_num).sum("invoice_items.quantity * invoice_items.unit_price")
+
+    items = Item.find(result.keys).map do |item|
+      {id: item.id, name: item.name}
+    end
 
   end
 
 
   def self.most_items(top_number)
-    self.joins(invoice_items: [invoice: [:transactions]]).where("transactions.result = 'success'").group("items.id").order("sum_invoice_items_quantity DESC").limit(top_number).sum("invoice_items.quantity")
+    result = self.joins(invoice_items: [invoice: [:transactions]]).where("transactions.result = 'success'").group("items.id").order("sum_invoice_items_quantity DESC").limit(top_number).sum("invoice_items.quantity")
 
-    # result.to_json
+    items = Item.find(result.keys).map do |item|
+      {id: item.id, name: item.name}
+    end
+
   end
-
 
   def best_day
-    #haven;t handled ties
-    #havent handled date format.
-    #spec has  2012-03-23T10:55:29.000Z vs
-    # 2012-03-20 23:57:05 UTC
-    invoice_items.joins(invoice: [:transactions]).where("transactions.result = 'success'").group("invoices.created_at").order("sum_invoice_items_quantity_all_invoice_items_unit_price DESC").limit(1).sum("invoice_items.quantity * invoice_items.unit_price")
+    best_day = invoice_items.joins(invoice: [:transactions]).where("transactions.result = 'success'").group("invoices.created_at").order("sum_invoice_items_quantity_all_invoice_items_unit_price DESC").limit(1).sum("invoice_items.quantity * invoice_items.unit_price").keys.first
+
+    {"best_day" => best_day}
   end
-
-
-
 
 end
